b48587d8657a6170af7e91ac1bf75571094c40b3
Dhrumil <sdhrumil2605@gmail.com>
2020-03-23 00:02:29 -0400

    code is ready

--- src/Board.js
+++ src/Board.js
@@ -2,36 +2,19 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
+
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick = {() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick = {() => this.props.onClick(i)}
       />
     );
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
